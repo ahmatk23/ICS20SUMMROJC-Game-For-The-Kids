92f3d4fec2d21b34db401f1d72249ece1b244d1f
nuumairottawa <43385047+nuumairottawa@users.noreply.github.com>
2018-12-13 15:09:05 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+mouseClicked = function() {
+    // Scene 2
+    background(94, 156, 39);
+    fill(7, 14, 145);
+    textSize(25);
+    text("wird games are wird", 10, 100);
+    image(getImage("avatars/avatar-team"), 80, 150);
+};
+
+// Scene 1
+background(255, 237, 255);
+fill(0, 255, 102);
+textSize(25);
+text("Wird game", 10, 200);
